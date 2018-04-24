8f64df9c1c02a00e9b5d9d6abc5454ad7f72f12f
coolli(李帅) <coolli@tencent.com>
2018-04-24 22:34:52 +0800

    release_3.0.3

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.1',
