bb6f68aebbc396b4a1b6a71da23ad85a393fa658
fxh123 <1083479794@qq.com>
2019-08-09 15:46:50 +0800

    tci

---
+++ examples/tci/v20190318/CreateLibrary.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import os
+
+from tencentcloud.common import credential
+from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
+# 导入对应产品模块的client models。
+
+from tencentcloud.tci.v20190318 import tci_client, models
+from tencentcloud.common.profile.client_profile import ClientProfile
+from tencentcloud.common.profile.http_profile import HttpProfile
+
+try:
+    # 实例化一个认证对象，入参需要传入腾讯云账户secretId，secretKey
+    cred = credential.Credential("", "")
+    #cred = credential.Credential(
+    #     os.environ.get("TENCENTCLOUD_SECRET_ID"),
+    #     os.environ.get("TENCENTCLOUD_SECRET_KEY"))
+
+    # 实例化一个http选项，可选的，没有特殊需求可以跳过。
+    httpProfile = HttpProfile()
+    httpProfile.reqMethod = "POST"  # post请求(默认为post请求)
+    httpProfile.reqTimeout = 30  # 请求超时时间，单位为秒(默认60秒)
+    httpProfile.endpoint = "tci.tencentcloudapi.com"  # 指定接入地域域名(默认就近接入)
+    httpProfile.keepAlive = True
+
+    # 实例化一个client选项，可选的，没有特殊需求可以跳过。
+    clientProfile = ClientProfile()
+    clientProfile.signMethod = "TC3-HMAC-SHA256"  # 指定签名算法(默认为HmacSHA256)
+    clientProfile.unsignedPayload = True                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+    clientProfile.httpProfile = httpProfile
+
+    client = tci_client.TciClient(cred, "", clientProfile)
+    req = models.CreateLibraryRequest()
+    req.LibraryName="testfxh1"
+
+    resp = client.CreateLibrary(req)
+
+    # 输出json格式的字符串回包
+    print("%s" % resp.to_json_string())
+
+except TencentCloudSDKException as err:
+    print("%s" % err)
+
+
