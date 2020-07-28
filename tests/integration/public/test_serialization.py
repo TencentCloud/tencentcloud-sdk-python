63d0c77dcee44945a2149f11abb9f7503dc19293
WANGMUXIAN <497569296@qq.com>
2020-07-28 18:09:05 +0800

    1、更改serialize方法 2、增加序列化单元测试

---
+++ tests/integration/public/test_serialization.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+import os
+
+from tencentcloud.common import credential
+from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
+# 导入对应产品模块的client models。
+from tencentcloud.cvm.v20170312 import cvm_client, models
+
+# 导入可选配置类
+from tencentcloud.common.profile.client_profile import ClientProfile
+from tencentcloud.common.profile.http_profile import HttpProfile
+
+
+def test_serialization():
+    try:
+        # 实例化一个认证对象，入参需要传入腾讯云账户secretId，secretKey
+        cred = credential.Credential(
+            os.environ.get("TENCENTCLOUD_SECRET_ID"),
+            os.environ.get("TENCENTCLOUD_SECRET_KEY"))
+
+        # 实例化一个http选项，可选的，没有特殊需求可以跳过。
+        httpProfile = HttpProfile()
+        # 如果需要指定proxy访问接口，可以按照如下方式初始化hp
+        # httpProfile = HttpProfile(proxy="http://用户名:密码@代理IP:代理端口")
+        httpProfile.reqMethod = "GET"  # post请求(默认为post请求)
+        httpProfile.reqTimeout = 30  # 请求超时时间，单位为秒(默认60秒)
+        httpProfile.endpoint = "cvm.ap-shanghai.tencentcloudapi.com"  # 指定接入地域域名(默认就近接入)
+
+        # 实例化一个client选项，可选的，没有特殊需求可以跳过。
+        clientProfile = ClientProfile()
+        clientProfile.signMethod = "TC3-HMAC-SHA256"  # 指定签名算法
+        clientProfile.language = "en-US"
+        clientProfile.httpProfile = httpProfile
+
+        # 实例化要请求产品(以cvm为例)的client对象，clientProfile是可选的。
+        client = cvm_client.CvmClient(cred, "ap-shanghai", clientProfile)
+
+        # 实例化一个cvm实例信息查询请求对象,每个接口都会对应一个request对象。
+        req = models.DescribeInstancesRequest()
+
+        # 填充请求参数,这里request对象的成员变量即对应接口的入参。
+        # 你可以通过官网接口文档或跳转到request对象的定义处查看请求参数的定义。
+        params = '''{
+            "Filters": [
+                {
+                    "Name": "zone",
+                    "Values": ["ap-shanghai-1", "ap-shanghai-2"]
+                },
+                {
+                    "Name": "instance-name",
+                    "Values": ["中文", "测试"]
+                }
+            ]
+        }'''
+        req.from_json_string(params)
+
+        # 通过client对象调用DescribeInstances方法发起请求。注意请求方法名与请求对象是对应的。
+        # 返回的resp是一个DescribeInstancesResponse类的实例，与请求对象对应。
+        resp = client.DescribeInstances(req)
+        # print(resp)
+        # 输出json格式的字符串回包
+        print(resp.to_json_string(indent=2))
+
+        # 也可以取出单个值。
+        # 你可以通过官网接口文档或跳转到response对象的定义处查看返回字段的定义。
+        print(resp.TotalCount)
+        assert resp.TotalCount >= 0
+
+    except TencentCloudSDKException as err:
+        print(err)
