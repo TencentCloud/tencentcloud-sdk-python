7f8600c265528c56d6e6f3eb53bb1e687b2a57a5
WANGMUXIAN <497569296@qq.com>
2021-05-14 15:37:18 +0800

    修改测试用例

--- tests/integration/public/test_serialization.py
+++ tests/integration/public/test_serialization.py
@@ -15,87 +15,95 @@ from tencentcloud.common.profile.http_profile import HttpProfile
 def test_serialization():
     try:
         mock_result = {
-            "InstanceSet": [
+            "InstanceSet":[
                 {
-                    "RenewFlag": "flag_demo",
-                    "Uuid": "88888888",
-                    "InstanceState": "RUNNING",
-                    "LatestOperationState": "SUCCESS",
-                    "LoginSettings": {
-                        "KeyIds": ["skey-*****"],
-                        "Password": None,
-                        "KeepImageLogin": None
+                    "RenewFlag":"flag_demo",
+                    "Uuid":"88888888",
+                    "InstanceState":"RUNNING",
+                    "LatestOperationState":"SUCCESS",
+                    "LoginSettings":{
+                        "KeyIds":[
+                            "skey-*****"
+                        ],
+                        "Password":None,
+                        "KeepImageLogin":None
                     },
-                    "IPv6Addresses": None,
-                    "RestrictState": "NORMAL",
-                    "ExpiredTime": "2021-07-03T06:30:28Z",
-                    "Memory": 4,
-                    "CreatedTime": "2017-07-03T06:30:25Z",
-                    "CPU": 2,
-                    "PublicIpAddresses": [
+                    "IPv6Addresses":None,
+                    "RestrictState":"NORMAL",
+                    "ExpiredTime":"2021-07-03T06:30:28Z",
+                    "Memory":4,
+                    "CreatedTime":"2017-07-03T06:30:25Z",
+                    "CPU":2,
+                    "RdmaIpAddresses":None,
+                    "PublicIpAddresses":[
                         "000.000.000.000"
                     ],
-                    "Tags": [],
-                    "InstanceId": "ins-8888888",
-                    "ImageId": "img-8888888",
-                    "StopChargingMode": "NOT_APPLICABLE",
-                    "InstanceChargeType": "PREPAID",
-                    "DataDisks": [
+                    "Tags":[
+
+                    ],
+                    "InstanceId":"ins-8888888",
+                    "ImageId":"img-8888888",
+                    "StopChargingMode":"NOT_APPLICABLE",
+                    "InstanceChargeType":"PREPAID",
+                    "DataDisks":[
                         {
-                            "DeleteWithInstance": None,
-                            "Encrypt": None,
-                            "DiskType": "CLOUD_BASIC",
-                            "KmsKeyId": None,
-                            "DiskSize": 10,
-                            "SnapshotId": None,
-                            "DiskId": "disk-8888888"
+                            "DeleteWithInstance":None,
+                            "Encrypt":None,
+                            "CdcId":None,
+                            "DiskType":"CLOUD_BASIC",
+                            "ThroughputPerformance":None,
+                            "KmsKeyId":None,
+                            "DiskSize":10,
+                            "SnapshotId":None,
+                            "DiskId":"disk-8888888"
                         }
                     ],
-                    "InstanceType": "S2.MEDIUM4",
-                    "SystemDisk": {
-                        "DiskSize": 50,
-                        "DiskId": "disk-8888888",
-                        "DiskType": "CLOUD_BASIC"
+                    "InstanceType":"S2.MEDIUM4",
+                    "SystemDisk":{
+                        "DiskSize":50,
+                        "DiskId":"disk-8888888",
+                        "CdcId":None,
+                        "DiskType":"CLOUD_BASIC"
                     },
-                    "Placement": {
-                        "HostIds": None,
-                        "ProjectId": 0,
-                        "HostId": None,
-                        "Zone": "ap-shanghai-2",
-                        "HostIps": None
+                    "Placement":{
+                        "HostIds":None,
+                        "ProjectId":0,
+                        "HostId":None,
+                        "Zone":"ap-shanghai-2",
+                        "HostIps":None
                     },
-                    "PrivateIpAddresses": [
+                    "PrivateIpAddresses":[
                         "000.000.000.000"
                     ],
-                    "OsName": "Ubuntu Server 16.04.1 LTS 64bit",
-                    "SecurityGroupIds": [
+                    "OsName":"Ubuntu Server 16.04.1 LTS 64bit",
+                    "SecurityGroupIds":[
                         "sg-8888888"
                     ],
-                    "CamRoleName": None,
-                    "InstanceName": "Redis",
-                    "DisasterRecoverGroupId": "",
-                    "VirtualPrivateCloud": {
-                        "SubnetId": None,
-                        "AsVpcGateway": False,
-                        "Ipv6AddressCount": None,
-                        "VpcId": None,
-                        "PrivateIpAddresses": None
+                    "CamRoleName":None,
+                    "InstanceName":"Redis",
+                    "DisasterRecoverGroupId":"",
+                    "VirtualPrivateCloud":{
+                        "SubnetId":None,
+                        "AsVpcGateway":False,
+                        "Ipv6AddressCount":None,
+                        "VpcId":None,
+                        "PrivateIpAddresses":None
                     },
-                    "LatestOperationRequestId": "0b9e9d12-7adb-414b-ae84-8888888",
-                    "InternetAccessible": {
-                        "PublicIpAssigned": None,
-                        "InternetChargeType": "BANDWIDTH_PREPAID",
-                        "BandwidthPackageId": None,
-                        "InternetMaxBandwidthOut": 1
+                    "LatestOperationRequestId":"0b9e9d12-7adb-414b-ae84-8888888",
+                    "InternetAccessible":{
+                        "PublicIpAssigned":None,
+                        "InternetChargeType":"BANDWIDTH_PREPAID",
+                        "BandwidthPackageId":None,
+                        "InternetMaxBandwidthOut":1
                     },
-                    "LatestOperation": "FailoverMigrateInstance",
-                    "HpcClusterId": None,
-                    "RdmaIpAddresses": None,
+                    "HpcClusterId":None,
+                    "LatestOperation":"FailoverMigrateInstance"
                 }
             ],
-            "TotalCount": 1,
-            "RequestId": "46fc254d-c862-401b-a2a0-8888888"
+            "TotalCount":1,
+            "RequestId":"46fc254d-c862-401b-a2a0-8888888"
         }
+
         result = dict(mock_result)
 
         result_json = json.dumps(result)
@@ -105,3 +113,5 @@ def test_serialization():
         assert json.loads(res_model.to_json_string()) == json.loads(result_json)
     except TencentCloudSDKException as err:
         print(err)
+
+test_serialization()
