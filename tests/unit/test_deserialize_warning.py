6a7cf8969e32cef47f713c3daa8d958ab3c1dd91
zhiqiangfan <zhiqiangfan@tencent.com>
2021-05-24 14:39:45 +0800

    fix test case

--- tests/unit/test_deserialize_warning.py
+++ tests/unit/test_deserialize_warning.py
@@ -1,23 +1,25 @@
 # -*- coding: utf-8 -*-
 import warnings
 
+import pytest
+
 from tencentcloud.cvm.v20170312 import models
 
-try:
-    warnings.simplefilter('error')
-    req = models.DescribeInstancesRequest()
 
+def test_deserialize_warning():
+    warnings.simplefilter('error')
+
+    req = models.DescribeInstancesRequest()
     params = '''{
         "Filters": [
             {
                 "Name": "zone",
                 "Values": ["ap-shanghai-1", "ap-shanghai-2"],
                 "a": "a"
-
             }
         ]
     }'''
-    req.from_json_string(params)
 
-except Exception as err:
-    assert "fileds are useless" in str(err)
+    with pytest.raises(Exception) as e:
+        req.from_json_string(params)
+    assert "fileds are useless" in str(e)
