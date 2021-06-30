e10fa75c141ed1eddb1804626a32db803e3d1220
zhiqiangfan <zhiqiangfan@tencent.com>
2021-06-30 16:47:10 +0800

    update sts test case

--- tests/integration/cvm/test_describe_instances_sts.py
+++ tests/integration/cvm/test_describe_instances_sts.py
@@ -1,9 +1,13 @@
 # -*- coding: utf8 -*-
 import os
 
+import pytest
+
 from tencentcloud.common import credential
 from tencentcloud.cvm.v20170312 import cvm_client, models
 
+@pytest.mark.skipif(not os.environ.get("TENCENTCLOUD_ROLE_ARN"),
+                    reason="TENCENTCLOUD_ROLE_ARN env var not found")
 def test_describe_instances():
     cred = credential.STSAssumeRoleCredential(
         os.environ.get("TENCENTCLOUD_SECRET_ID"),
