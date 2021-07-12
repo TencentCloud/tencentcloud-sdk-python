25ef5c6066f6cbfd85eae41bad73613242e217a7
zhiqiangfan <zhiqiangfan@tencent.com>
2021-07-12 21:25:21 +0800

    clean example code

--- examples/common_client/describe_instances.py
+++ examples/common_client/describe_instances.py
@@ -1,38 +1,40 @@
 # -*- coding: utf-8 -*-
+# Copyright 2017-2021 Tencent Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import os
 import json
 
 from tencentcloud.common.common_client import CommonClient
 from tencentcloud.common import credential
 from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
-
-# 导入可选配置类
 from tencentcloud.common.profile.client_profile import ClientProfile
 from tencentcloud.common.profile.http_profile import HttpProfile
 
 try:
-    # 实例化一个临时认证对象，入参需要传入腾讯云账户secretId，secretKey
     cred = credential.Credential(
         os.environ.get("TENCENTCLOUD_SECRET_ID"),
         os.environ.get("TENCENTCLOUD_SECRET_KEY"))
 
-    # 实例化一个http选项，可选的，没有特殊需求可以跳过。
     httpProfile = HttpProfile()
-    # 如果需要指定proxy访问接口，可以按照如下方式初始化hp
-    # httpProfile = HttpProfile(proxy="http://用户名:密码@代理IP:代理端口")
-    httpProfile.reqMethod = "GET"  # post请求(默认为post请求)
-    httpProfile.reqTimeout = 30    # 请求超时时间，单位为秒(默认60秒)
-    httpProfile.endpoint = "cvm.ap-shanghai.tencentcloudapi.com"  # 指定接入地域域名(默认就近接入)
-
-    # 实例化一个client选项，可选的，没有特殊需求可以跳过。
+    # 域名首段必须和下文中CommonClient初始化的产品名严格匹配
+    httpProfile.endpoint = "cvm.tencentcloudapi.com"
     clientProfile = ClientProfile()
-    clientProfile.signMethod = "TC3-HMAC-SHA256"  # 指定签名算法
-    clientProfile.language = "en-US"
     clientProfile.httpProfile = httpProfile
 
     # 实例化要请求的common client对象，clientProfile是可选的。
     common_client = CommonClient("cvm", '2017-03-12', cred, "ap-shanghai", profile=clientProfile)
-
+    # 接口参数作为json字典传入，得到的输出也是json字典，请求失败将抛出异常
     print(common_client.call_json("DescribeInstances", {"Limit": 10}))
 except TencentCloudSDKException as err:
     print(err)
