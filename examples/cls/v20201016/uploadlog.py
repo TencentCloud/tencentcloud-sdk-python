d3649d97190f11a08d10682ee936f8dd80bb0a6e
lleiyyang <39645792+lleiyyang@users.noreply.github.com>
2021-11-25 10:51:03 +0800

    支持lz4压缩 (#118)
    * 支持lz4压缩
    
    Co-authored-by: samlyang <samlyang@tencent.com>

--- examples/cls/v20201016/uploadlog.py
+++ examples/cls/v20201016/uploadlog.py
@@ -12,10 +12,26 @@ from tencentcloud.common.profile.http_profile import HttpProfile
 import pb
 
 try:
-    # 实例化一个认证对象，入参需要传入腾讯云账户secretId，secretKey
-    cred = credential.Credential(
-        os.environ.get("TENCENTCLOUD_SECRET_ID"),
-        os.environ.get("TENCENTCLOUD_SECRET_KEY"))
+    ##安装lz4: pip install lz4==0.8.2
+    import lz4
+
+    if not hasattr(lz4, 'loads') or not hasattr(lz4, 'dumps'):
+        lz4 = None
+    else:
+        def lz_compresss(data):
+            return lz4.dumps(data)[4:]
+
+except ImportError:
+    lz4 = None
+
+try:
+    compressType = "xxx" # 压缩方式, 目前只支持lz4, 客户根据需要填写(空字符串意味不压缩)
+    region = "xxxx" # 需要根据客户的实际地域自行填写
+    topicId = "xxxxxx-xxxxxx-xxxxxx-xxxxxx" # 这里需要使用客户实际的topicId，不能输入topicname
+    hashKey = "" # 可选参数，具体参考官方文档：https://cloud.tencent.com/document/product/614/59470
+    credential = credential.Credential(
+        os.Getenv("TENCENTCLOUD_SECRET_ID"),
+        os.Getenv("TENCENTCLOUD_SECRET_KEY"))
 
     # 实例化一个http选项，可选的，没有特殊需求可以跳过。
     httpProfile = HttpProfile()
@@ -29,17 +45,22 @@ try:
     clientProfile.signMethod = "TC3-HMAC-SHA256"  # 指定签名算法(默认为HmacSHA256)
     clientProfile.httpProfile = httpProfile
 
-    client = common_client.CommonClient("cls", '2020-10-16', cred, "ap-beijing", clientProfile)
+    client = common_client.CommonClient("cls", '2020-10-16', cred, region, clientProfile)
+    body = pb.pb_gen(1, 1)
+    if compressType == "lz4":
+        if lz4 is None or body == "":
+            print("lz4 exception")
+            exit(1)
+        else:
+            body = lz_compresss(body)
+
     headers = {
-        # 使用对应地域下真实存在的日志主题ID
-        "X-CLS-TopicId": "xxxxf2e2-166c-4174-9473-b6a6dfca6f6e",
-        # 主题分区，https://cloud.tencent.com/document/product/614/39259
-        # 取值00000000000000000000000000000000，ffffffffffffffffffffffffffffffff
-        "X-CLS-HashKey": "0fffffffffffffffffffffffffffffff",
-        # 压缩类型
-        "X-CLS-CompressType": "",
+        "X-CLS-TopicId": topicId,
+        "X-CLS-HashKey": hashKey,
+        "X-CLS-CompressType": compressType,
     }
-    resp = client.call_octet_stream("UploadLog", headers, pb.pb_gen(1,1))
+
+    resp = client.call_octet_stream("UploadLog", headers, body)
 
     # 输出json格式的字符串回包
     print("%s" % resp)
