b04bbe9cb41933cb640e61aba9498dd64a0d94ee
HS-Gray <1978489546@qq.com>
2022-09-01 17:40:55 +0800

    product package requirement

--- package.py
+++ package.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import sys
 import argparse
+from tencentcloud import __version__
 
 
 README = '''============================
@@ -44,6 +45,7 @@ ROOT = os.path.dirname(__file__)
 
 setup(
     name='tencentcloud-sdk-python-%s',
+    %s,
     version=tencentcloud.__version__,
     description='Tencent Cloud %s SDK for Python',
     long_description=open('README.rst').read(),
@@ -112,8 +114,12 @@ def build_and_install_package(mod, upload):
 
 
 def mk_config_file(temp_dir, mod):
+    if mod == 'common':
+        required = 'install_requires=["requests>=2.27.0"]'
+    else:
+        required = 'install_requires=["tencentcloud-sdk-python-common==%s"]' % __version__
     with open(os.path.join(temp_dir, 'setup.py'), 'w') as f:
-        f.write(SETUP % (mod, mod.capitalize()))
+        f.write(SETUP % (mod, required, mod.capitalize()))
     with open(os.path.join(temp_dir, 'README.rst'), 'w') as f:
         f.write(README % (mod.capitalize(), mod, mod))
     with open(os.path.join(temp_dir, 'setup.cfg'), 'w') as f:
