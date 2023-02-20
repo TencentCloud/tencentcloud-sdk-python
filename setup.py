10da0c6d235319e0f24ece7c7881287c10e54aeb
zhiqiangfan <zhiqiangfan@tencent.com>
2023-02-20 19:39:15 +0800

    rm 3.6 and add 3.10

--- setup.py
+++ setup.py
@@ -30,9 +30,9 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 )
