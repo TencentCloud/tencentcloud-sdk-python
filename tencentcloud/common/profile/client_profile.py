78824f94883ea4d6eadaaa6e9fbea2a749b8259a
HS-Gray <1978489546@qq.com>
2024-03-19 16:58:00 +0800

    fix import

--- tencentcloud/common/profile/client_profile.py
+++ tencentcloud/common/profile/client_profile.py
@@ -11,8 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import warnings
 import re
+import warnings
 from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
 from tencentcloud.common.profile.http_profile import HttpProfile
 
