40657fdd00fac4bd23f86caf47a5fb28c4f2cedf
zzc <1378113190@qq.com>
2025-05-27 14:23:23 +0800

    fix(credential): resolve config file path issue on Windows

--- tencentcloud/common/credential.py
+++ tencentcloud/common/credential.py
@@ -283,9 +283,8 @@ class ProfileCredential(object):
     """
 
     def get_credential(self):
-        home_path = os.environ.get('HOME') or os.environ.get('HOMEPATH')
-        if os.path.exists(home_path + "/.tencentcloud/credentials"):
-            file_path = home_path + "/.tencentcloud/credentials"
+        if os.path.exists(os.path.expanduser("~/.tencentcloud/credentials")):
+            file_path = os.path.expanduser("~/.tencentcloud/credentials")
         elif os.path.exists("/etc/tencentcloud/credentials"):
             file_path = "/etc/tencentcloud/credentials"
         else:
