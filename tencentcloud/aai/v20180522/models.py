ef36baa4f4d8403b607bc242e766996e487d456f
tencentcloudapi <tencentcloudapi@tencent.com>
2025-07-05 23:55:44 +0800

    release 3.0.1416

--- tencentcloud/aai/v20180522/models.py
+++ tencentcloud/aai/v20180522/models.py
@@ -1,5 +1,5 @@
 # -*- coding: utf8 -*-
-# Copyright (c) 2017-2021 THL A29 Limited, a Tencent company. All Rights Reserved.
+# Copyright (c) 2017-2025 Tencent. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
