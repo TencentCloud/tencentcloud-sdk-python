b7c0536fdc908cfb9b48bd943dd0fbf8c43eb225
arongao <arongao@tencent.com>
2025-07-25 19:10:48 +0800

    tke测试中设置环境变量

--- tests/integration/common/test_temp_credentials.py
+++ tests/integration/common/test_temp_credentials.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 import json
 import os
+import requests
+import tempfile
 
 import pytest
+
+from contextlib import contextmanager
+from urllib.parse import urlparse
 
 from tencentcloud.common import credential
 from tencentcloud.common.profile.client_profile import ClientProfile
@@ -11,6 +16,7 @@ from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentClo
 from tencentcloud.sts.v20180813 import sts_client, models
 from tencentcloud.cvm.v20170312 import cvm_client
 from tencentcloud.cvm.v20170312 import models as cvm_models
+from tencentcloud.common.credential import STSAssumeRoleCredential, OIDCRoleArnCredential, DefaultTkeOIDCRoleArnProvider
 
 reqmethod_list = ['GET', 'POST']
 
@@ -81,3 +87,147 @@ def test_temp_credential_invalid():
             resp = client.DescribeInstancesOperationLimit(req)
         except TencentCloudSDKException as err:
             assert err.code == 'AuthFailure.TokenFailure'
+
+
+@contextmanager
+def mock_requests():
+    real_request = requests.Session.request
+    req_args = {}
+
+    def interceptor(self, method, url, **kwargs):
+        req_args["method"] = method
+        req_args["url"] = url
+        return real_request(self, method, url, **kwargs)
+
+    requests.Session.request = interceptor
+
+    yield req_args
+    
+    requests.Session.request = real_request
+
+
+def parse_host(url):
+    return urlparse(url).hostname
+
+
+def test_sts_credential_with_default_endpoint():
+    expected_host = "sts.tencentcloudapi.com"
+
+    cred = STSAssumeRoleCredential(
+        "test-secret-id",
+        "test-secret-key",
+        "test-role-arn",
+        "test-role-session-name",
+        7000
+    )
+
+    with mock_requests() as req_args:
+        try:
+            cred.get_sts_tmp_role_arn()
+        except TencentCloudSDKException:
+            pass
+
+        assert parse_host(req_args["url"]) == expected_host
+
+
+def test_sts_credential_with_set_endpoint():
+    expected_host = "sts.internal.tencentcloudapi.com"
+
+    cred = STSAssumeRoleCredential(
+        "test-secret-id",
+        "test-secret-key",
+        "test-role-arn",
+        "test-role-session-name",
+        7000,
+        "sts.internal.tencentcloudapi.com"
+    )
+
+    with mock_requests() as req_args:
+        try:
+            cred.get_sts_tmp_role_arn()
+        except TencentCloudSDKException:
+            pass
+
+        assert parse_host(req_args["url"]) == expected_host
+
+
+def test_oidc_credential_with_deault_endpoint():
+    expected_host = "sts.tencentcloudapi.com"
+
+    cred = OIDCRoleArnCredential(
+        region="test-region",
+        provider_id="test-provider-id",
+        web_identity_token="test-web-identity-token",
+        role_arn="test-role-arn",
+        role_session_name="test-role-session-name"
+    )
+
+    with mock_requests() as req_args:
+        try:
+            cred.refresh()
+        except TencentCloudSDKException:
+            pass
+
+        assert parse_host(req_args["url"]) == expected_host
+
+
+def test_oidc_credential_with_set_endpoint():
+    expected_host = "sts.internal.tencentcloudapi.com"
+
+    cred = OIDCRoleArnCredential(
+        region="test-region",
+        provider_id="test-provider-id",
+        web_identity_token="test-web-identity-token",
+        role_arn="test-role-arn",
+        role_session_name="test-role-session-name",
+        endpoint="sts.internal.tencentcloudapi.com" 
+    )
+
+    with mock_requests() as req_args:
+        try:
+            cred.refresh()
+        except TencentCloudSDKException:
+            pass
+
+        assert parse_host(req_args["url"]) == expected_host
+
+
+def test_tke_oidc_credential_with_default_endpoint():
+    expected_host = "sts.tencentcloudapi.com"
+
+    os.environ["TKE_REGION"] = "test_tke"
+    os.environ["TKE_PROVIDER_ID"] = "test_tke"
+    os.environ["TKE_ROLE_ARN"] = "test_tke"
+
+    with tempfile.NamedTemporaryFile(mode="w+") as f:
+        os.environ["TKE_WEB_IDENTITY_TOKEN_FILE"] = f.name
+        cred = DefaultTkeOIDCRoleArnProvider().get_credential()
+
+        with mock_requests() as req_args:
+            try:
+                cred.refresh()
+            except TencentCloudSDKException:
+                pass
+
+            assert parse_host(req_args["url"]) == expected_host
+
+
+def test_tke_oidc_credential_with_set_endpoint():
+    expected_host = "sts.internal.tencentcloudapi.com"
+
+    os.environ["TKE_REGION"] = "test_tke"
+    os.environ["TKE_PROVIDER_ID"] = "test_tke"
+    os.environ["TKE_ROLE_ARN"] = "test_tke"
+
+    with tempfile.NamedTemporaryFile(mode="w+") as f:
+        os.environ["TKE_WEB_IDENTITY_TOKEN_FILE"] = f.name
+        cred = DefaultTkeOIDCRoleArnProvider().get_credential()
+        cred.endpoint = "sts.internal.tencentcloudapi.com"
+
+        with mock_requests() as req_args:
+            try:
+                cred.refresh()
+            except TencentCloudSDKException:
+                pass
+
+            assert parse_host(req_args["url"]) == expected_host
